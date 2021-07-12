23fa124a0719de98d003b63b140301da1a99a89a
Anthony José <anthonyj2017@icloud.com>
2021-07-12 19:28:43 -0300

    Add coordinates and create MKMapView

--- MapKitSegmentControl/ContentView.swift
+++ MapKitSegmentControl/ContentView.swift
@@ -6,15 +6,53 @@
 //
 
 import SwiftUI
-import CoreData
+import MapKit
+import UIKit
+
+
+struct LocationItem {
+    var title: String
+    var coordinates: CLLocationCoordinate2D
+}
 
 struct ContentView: View {
+    
+    @State private var selectedSegment = 0
+    @State private var locations = [
+        LocationItem(title: "Utah", coordinates: CLLocationCoordinate2D(latitude: 40.7608, longitude: -111.8910)),
+        LocationItem(title: "California", coordinates: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194))
+    ]
+    
     var body: some View {
         NavigationView {
             VStack {
-                Text("Content")
+                HStack {
+                    Picker("Escolha um lugar", selection: $selectedSegment) {
+                        ForEach(0 ..< locations.count) {
+                            Text(self.locations[$0].title).tag($0)
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                     .padding()
+                }
+                // mapview would go here
+//                Text("\(self.locations[selectedSegment].coordinates.latitude)     \(self.locations[selectedSegment].coordinates.longitude)")
+                MapView(coordinates: locations[selectedSegment].coordinates)
             }
             .navigationTitle("Locais")
         }
+    }
+}
+
+struct MapView: UIViewRepresentable {
+    var coordinates: CLLocationCoordinate2D
+    
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let span = MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        let region = MKCoordinateRegion(center: coordinates, span: span)
+        uiView.setRegion(region, animated: true)
     }
 }
